96bfef62142c0aed5ac89628c0795f6649cd120b
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-03-28 19:10:13 +0530

    Ruby update version

--- ruby/install.sh
+++ ruby/install.sh
@@ -14,10 +14,10 @@ source /usr/local/rvm/scripts/rvm
 rvm requirements
 
 echo "================= Installing default ruby ==================="
-rvm install 2.4.1
+rvm install 2.5.0
 
 # tell rvm to use this version as default
-rvm use 2.4.1 --default
+rvm use 2.5.0 --default
 
 #update gems to current
 rvm rubygems current
