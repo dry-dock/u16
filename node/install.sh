4ecd4d0171677a67aaf0b54265463e47911f99ed
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-03-29 12:12:59 +0530

    nodejs updated version

--- node/install.sh
+++ node/install.sh
@@ -11,7 +11,7 @@ echo "source ~/.nvm/nvm.sh" >> /etc/drydock/.env
 
 echo "================= Installing nodejs 8.11.0 ================="
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs=8.11.0*-2nodesource1~xenial1
+sudo apt-get install -y nodejs=8.11.0*
 
 echo "================= Installing latest yarn ==================="
 sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
