5155a5907631c0c66d2509cd7620b9f979352039
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-03-29 12:20:10 +0530

    Nodejs

--- node/install.sh
+++ node/install.sh
@@ -10,7 +10,7 @@ export NVM_DIR="/root/.nvm"
 echo "source ~/.nvm/nvm.sh" >> /etc/drydock/.env
 
 echo "================= Installing nodejs 8.11.0 ================="
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sSL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs=8.11.0*
 
 echo "================= Installing latest yarn ==================="
