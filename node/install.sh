7d8e767525db12c40523a691b4d98052a9b26167
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-10 00:33:24 +0530

    Removing patch version from yarn

--- node/install.sh
+++ node/install.sh
@@ -17,4 +17,4 @@ echo "================= Installing latest yarn ==================="
 sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get install -y yarn=1.6.0
+sudo apt-get install -y yarn=1.6*
