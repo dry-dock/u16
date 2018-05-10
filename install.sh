6fc93810e74f07c4d334c9af7b665be0d0aea850
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-10 11:23:56 +0530

    Removing patch version from gcloud u16

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ echo "================= Adding gcloud ============"
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo apt-get update && sudo apt-get install -q google-cloud-sdk=200.0.0
+sudo apt-get update && sudo apt-get install -q google-cloud-sdk=200.0*
 
 KUBECTL_VERSION=1.10.0
 echo "================= Adding kubectl $KUBECTL_VERSION ==================="
