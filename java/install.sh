f92b27f43008bd443af392cc153e8395fff83431
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-08 18:42:27 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -17,7 +17,7 @@ update-alternatives --set javaws /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
 
 echo "================ Installing oracle-java10-installer ================="
 echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-add-apt-repository -y ppa:webupd10team/java
+sudo add-apt-repository ppa:linuxuprising/java
 apt-get update
 apt-get install -y -q oracle-java10-installer
 update-alternatives --set java /usr/lib/jvm/java-10-oracle/jre/bin/java
