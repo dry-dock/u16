88f8f2037039e204295fac60c1c43304ef535fa5
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-08 16:40:20 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -4,24 +4,24 @@ echo "================= Installing default-jdk & jre ==================="
 apt-get install -q -y  default-jre=2:1.8*
 apt-get install -q -y default-jdk=2:1.8*
 
-echo "================= Installing openjdk-8-jdk ==================="
+echo "================= Installing openjdk-10-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
-apt-get install -q -y openjdk-8-jdk
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
+apt-get install -q -y openjdk-10-jdk
+update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
 add-apt-repository ppa:maarten-fonville/ppa
 apt-get update
-apt-get install -y -q icedtea-8-plugin=1.6*
-update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javaws
+apt-get install -y -q icedtea-10-plugin=1.6*
+update-alternatives --set javaws /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/javaws
 
-echo "================ Installing oracle-java8-installer ================="
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-add-apt-repository -y ppa:webupd8team/java
+echo "================ Installing oracle-java10-installer ================="
+echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+add-apt-repository -y ppa:webupd10team/java
 apt-get update
 apt-get install -y -q oracle-java8-installer
-update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
-update-alternatives --set javaws /usr/lib/jvm/java-8-oracle/jre/bin/javaws
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/lib/jvm/java-8-oracle/jre/bin' >> /etc/drydock/.env
+update-alternatives --set java /usr/lib/jvm/java-10-oracle/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-10-oracle/bin/javac
+update-alternatives --set javaws /usr/lib/jvm/java-10-oracle/jre/bin/javaws
+echo 'export JAVA_HOME=/usr/lib/jvm/java-10-oracle' >> /etc/drydock/.env
+echo 'export PATH=$PATH:/usr/lib/jvm/java-10-oracle/jre/bin' >> /etc/drydock/.env
