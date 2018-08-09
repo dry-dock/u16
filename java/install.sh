c29321608e9986c31c7a1ef222cc0001dc90dc29
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-09 13:49:19 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -1,27 +1,20 @@
 #!/bin/bash -e
 
 echo "================= Installing default-jdk & jre ==================="
-apt-get install -q -y  default-jre=2:1.8*
-apt-get install -q -y default-jdk=2:1.8*
+apt-get install -q default-jre=2:1.7*
+apt-get install -q default-jdk=2:1.7*
 
 echo "================= Installing openjdk-10-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
-apt-get install -q -y openjdk-10-jdk
-update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
-add-apt-repository ppa:maarten-fonville/ppa
-apt-get update
-apt-get install -y -q icedtea-10-plugin=1.6*
-update-alternatives --set javaws /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/javaws
+apt-get install -y -q openjdk-10-jdk
+
 
 echo "================ Installing oracle-java10-installer ================="
-echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-sudo add-apt-repository ppa:linuxuprising/java
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+add-apt-repository -y ppa:linuxuprising/java
 apt-get update
-apt-get install -y -q oracle-java10-installer
-update-alternatives --set java /usr/lib/jvm/java-10-oracle/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-10-oracle/bin/javac
-update-alternatives --set javaws /usr/lib/jvm/java-10-oracle/jre/bin/javaws
+apt-get install -q -y oracle-java10-installer
+sudo apt-get install oracle-java10-set-default
 echo 'export JAVA_HOME=/usr/lib/jvm/java-10-oracle' >> /etc/drydock/.env
 echo 'export PATH=$PATH:/usr/lib/jvm/java-10-oracle/jre/bin' >> /etc/drydock/.env
