8e4d99af52332a6586bd8d2d05c45d98998b2bca
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-09 13:45:15 +0530

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -18,4 +18,4 @@ echo "================= Installing latest yarn ==================="
 sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get install -y yarn=1.7*
+sudo apt-get install -y yarn=1.9*
