3f71d3df03c60c5eae031ca97cf6a852c5ed00c9
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-10-08 14:02:43 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -4,11 +4,6 @@ echo "================ Installing openjdk11-installer ================="
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt install -y openjdk-11-jdk --allow-unauthenticated
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-openjdk-amd64/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac 1
-sudo update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac
-
 
 echo "================ Installing oracle-java11-installer ================="
 export ORACLEJDK_VERSION=11
@@ -16,5 +11,9 @@ mkdir -p /usr/lib/jvm && cd /usr/lib/jvm
 wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.deb
 tar -xzf jdk-"$ORACLEJDK_VERSION"_linux-x64_bin.tar.gz
 mv jdk-"$ORACLEJDK_VERSION"/ java-11-oraclejdk-amd64
-echo 'export JAVA_HOME=/usr/lib/jvm/java-11-oracle' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/lib/jvm/java-11-oracle/jre/bin' >> /etc/drydock/.env
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-oraclejdk-amd64' >> /etc/drydock/.env
+echo 'export PATH=$PATH:/usr/lib/jvm/java-11-oraclejdk-amd64/bin' >> /etc/drydock/.env
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-oraclejdk-amd64/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-oraclejdk-amd64/bin/javac 1
+sudo update-alternatives --set java /usr/lib/jvm/java-11-oraclejdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-11-oraclejdk-amd64/bin/javac
