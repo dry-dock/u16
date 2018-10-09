3ad6803df216b1aa173cf8d3d2adb528bd12e3ff
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-10-09 13:24:34 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-echo "================ Installing oracle-java11-installer ================="
 export ORACLEJDK_VERSION=11
+echo "================ Installing oracle-java$ORACLEJDK_VERSION-installer ================="
 mkdir -p /usr/lib/jvm && cd /usr/lib/jvm
-wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-"$ORACLEJDK_VERSION"_linux-x64_bin.tar.gz
 tar -xzf jdk-"$ORACLEJDK_VERSION"_linux-x64_bin.tar.gz
 mv jdk-"$ORACLEJDK_VERSION"/ java-"$ORACLEJDK_VERSION"-oraclejdk-amd64
 echo 'export JAVA_HOME=/usr/lib/jvm/java-"$ORACLEJDK_VERSION"-oraclejdk-amd64' >> /etc/drydock/.env
@@ -12,8 +12,8 @@ echo 'export PATH=$PATH:/usr/lib/jvm/java-"$ORACLEJDK_VERSION"-oraclejdk-amd64/b
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-"$ORACLEJDK_VERSION"-oraclejdk-amd64/bin/java 1
 sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-"$ORACLEJDK_VERSION"-oraclejdk-amd64/bin/javac 1
 
-
-echo "================ Installing openjdk11-installer ================="
+export OPEN_JDK=11
+echo "================ Installing openjdk$OPEN_JDK-installer ================="
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt install -y openjdk-11-jdk --allow-unauthenticated
+sudo apt install -y openjdk-"$OPEN_JDK"-jdk --allow-unauthenticated
